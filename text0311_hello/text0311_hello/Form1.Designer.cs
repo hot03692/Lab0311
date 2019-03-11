24012dd141092f009e96ae3d7df2374a052df471
柏宇 陳 <Hot03692@outlook.com>
2019-03-11 12:09:25 +0800

    change button x,y

--- text0311_hello/text0311_hello/Form1.Designer.cs
+++ text0311_hello/text0311_hello/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(114, 33);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(154, 67);
             this.button1.TabIndex = 0;
@@ -45,7 +45,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(689, 383);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
